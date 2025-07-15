f452c4510d54eea58eacdc41c0094a804de601dc
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-15 20:22:23 +0900

    function.py로 curriculum.py 파일의 함수 중 일부를 통합 및 #1 학습 페이지 개발 시작

--- function.py
+++ function.py
@@ -36,19 +36,20 @@ def load_user_profile(user_id):
     profiles = load_user_profiles()
     return profiles.get("users", {}).get(user_id)
 
-def init_curriculum():
+def init_curriculum() : 
     # JSON 파일이 존재하지 않으면 초기화를 수행
-    if not curriculum_path.exists():
+    if not curriculum_path.exists() : 
         # 파일을 쓰기 모드로 열고 저장 준비
-        with open(curriculum_path, "w", encoding="utf-8") as f:
-            json.dump({}, f, ensure_ascii=False, indent=4)
+        with open(curriculum_path, "w", encoding = "utf-8") as f : 
+            json.dump({}, f, ensure_ascii = False, indent = 4)
 
-def load_curriculums() : 
-    # 파일이 존재하는지 확인 후 열어서 내용 읽기
-    if curriculum_path.exists() : 
-        with open(curriculum_path, "r", encoding = "utf-8") as f: 
+# 유저 커리큘럼 목록을 읽어서 반환하는 함수
+def load_curriculum():
+    if curriculum_path.exists():
+        with open(curriculum_path, "r", encoding="utf-8") as f:
             return json.load(f)
-    
+    else : 
+        init_curriculum()
     return {}
     
 
--- pages/learn.py
+++ pages/learn.py
@@ -1,1 +1,9 @@
 # 커리큘럼 선택하면, 여기서 이제 학습을 진행하고 그걸 또 ai가 도와주는 페이지임
+import streamlit as st
+import json
+from pathlib import Path
+from function import *
+
+DATA_DIR = Path("data")
+DATA_DIR.mkdir(exist_ok=True)
+curriculum_path = DATA_DIR / "curriculums.json"
